7d7bd27ce400dc9a691fa251361f1f03901ca77d
kenboydz <kenboydz@gmail.com>
2020-03-04 20:11:36 +0800

    更新了 els.slx 和 els_lib;

---
+++ lib/wrap/include/els_math_tool.h
@@ -0,0 +1,19 @@
+#ifndef __ELS_MATH_TOOL_H__
+#define __ELS_MATH_TOOL_H__
+
+
+// for els_pll
+#define _DECLARE_CODE
+#define _Q          Q17
+#define _POSTFIX    _Q17
+#include "_els_math_tool_template.h"
+
+
+// for els_svm
+#define _DECLARE_CODE
+#define _Q          Q26
+#define _POSTFIX    _Q26
+#include "_els_math_tool_template.h"
+
+
+#endif  // __ELS_MATH_TOOL_H__
